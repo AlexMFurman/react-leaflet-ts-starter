b01be4dca4679e9c4fe084f798b60d65cc0b6991
Alex Furmanm <alex.furman@live.com>
2019-01-26 18:21:45 -0500

    Fixed Declarations

--- src/map.tsx
+++ src/map.tsx
@@ -1,17 +1,16 @@
-import React, { SuspenseProps } from "react";
-import ReactDOM from "react-dom";
-import * as L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import React, { SuspenseProps } from 'react';
+import ReactDOM from 'react-dom';
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default class Map extends React.Component {
-  
-  map ?: L.Map | null;
+  map?: L.Map;
   componentDidMount() {
     var map = (this.map = L.map(ReactDOM.findDOMNode(this) as HTMLElement, {
       minZoom: 2,
       maxZoom: 20,
       layers: [
-        L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution:
             '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
         })
@@ -19,18 +18,19 @@ export default class Map extends React.Component {
       attributionControl: false
     }));
 
-    map.on("click", this.onMapClick);
+    map.on('click', this.onMapClick);
     map.fitWorld();
   }
 
   componentWillUnmount() {
     if (!this.map) return;
-    this.map.off("click", this.onMapClick);
-    this.map = null;
+    this.map.off('click', this.onMapClick);
+    this.map = undefined;
   }
 
-  onMapClick = () => {
-    alert(this.map);
+  onMapClick = (ev: any) => {
+    var latlng = (this.map as L.Map).mouseEventToLatLng(ev.originalEvent);
+    console.log(latlng.lat + ', ' + latlng.lng);
   };
 
   render() {
